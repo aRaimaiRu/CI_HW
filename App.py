f9265ce437f6db200f4b110d6f281a6127072c8a
Kittipat Poosrimoung <first1801@hotmail.com>
2021-05-05 18:35:39 +0700

    use render template

--- App.py
+++ App.py
@@ -7,14 +7,14 @@ import torch.nn as nn
 from torchvision import models
 import torchvision.transforms as T
 from PIL import Image
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request ,url_for,render_template, redirect
 from flask_cors import CORS,cross_origin
+import copy
 print(torch.__version__)
 
 
 myPredictClass = ['buildings' ,'forest' ,'glacier' ,'mountain', 'sea', 'street']
-Predictclass = ["Not Mosquito", "Mosquito"]
-Randomspecies = ["Mosquito", "Anopheles"]
+
 
 app = Flask(__name__)
 # imagenet_class_index = json.load(open('<PATH/TO/.json/FILE>/imagenet_class_index.json'))
@@ -47,27 +47,51 @@ def get_prediction(image_bytes):
     print("predicted_idx",predicted_idx);
     return myPredictClass[int(predicted_idx)]
 
-@app.route('/')
+@app.route('/hello')
 @cross_origin()
 def hello():
     return 'Hello'
+
+@app.route('/', methods=['GET', 'POST'])
+@cross_origin()
+def upload_file():
+    if request.method == 'POST':
+        # Check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # Check if no file was submitted to the HTML form
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+        if file and file.filename.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
+            filename = file.filename
+            
+            file = request.files['file']
+            img_bytes = file.read()
+            output = get_prediction(image_bytes=img_bytes)
+            image = Image.open(io.BytesIO(img_bytes))
+            image.save(os.path.join('static', filename))
+            path_to_image = url_for('static', filename = filename)
+            result = {
+                'output': output,
+                'path_to_image': path_to_image,
+                'size':150
+            }
+            return render_template('show.html', result=result,bg=url_for('static', filename = "BG.png" ))
+    return render_template('index.html',bg=url_for('static', filename = "BG.png" ))
 
 
 @app.route('/predict', methods=['POST'])
 @cross_origin()
 def predict():
-    
     file = request.files['file']
     img_bytes = file.read()
     # print(img_bytes)
     class_id  = get_prediction(image_bytes=img_bytes)
-    if(class_id == "Mosquito"):
-        resultRandom = random.choices(Randomspecies, weights=(30,70), k=1)
-        # print(resultRandom[0])
-        return jsonify({'Predict': resultRandom[0]})
-    else:
-        return jsonify({'Predict': class_id})
-    # return jsonify({'Predict': class_id})
+    return jsonify({'Predict': class_id})
+
 
 
 if __name__ == '__main__':
